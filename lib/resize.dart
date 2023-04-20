86be25b7595c95fd401a1a055f9409c668603e66
Maria Grigoryan <110464000+mariagrig2004@users.noreply.github.com>
2023-04-20 15:12:10 +0400

    Add files via upload

---
+++ lib/resize.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+export 'package:untitled/table/resize.dart';
+
+
+class ResizebleWidget extends StatefulWidget {
+  ResizebleWidget({super.key, required Container child});
+
+  @override
+  _ResizebleWidgetState createState() => _ResizebleWidgetState();
+}
+
+const ballDiameter = 80.0;
+bool isSelected = false;
+
+class _ResizebleWidgetState extends State<ResizebleWidget> {
+  //bool isSelected = false;
+  double height = 200;
+  double width = 250;
+
+  double top = 0;
+  double left = 0;
+
+  void onDrag(double dx, double dy) {
+    var newHeight = height + dy;
+    var newWidth = width + dx;
+
+    setState(() {
+      height = newHeight > 0 ? newHeight : 0;
+      width = newWidth > 0 ? newWidth : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Positioned(
+          top: top,
+          left: left,
+          child: Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+              //border: Border.all(color: Colors.black, width: 1),
+              border: Border.all(color: isSelected ? Colors.blue : Colors.black, width: 1),
+            ),
+          ),
+        ),
+        // top left
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + dy) / 2;
+              var newHeight = height - 2 * mid;
+              var newWidth = width - 2 * mid;
+
+              setState(() {
+                top = top + (height - newHeight);
+                left = left + 2 * mid;
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        // top middle
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newHeight = height - dy;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                top = top + dy;
+              });
+            },
+          ),
+        ),
+        // top right
+        Positioned(
+          top: top - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + (dy * -1)) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                top = top + (height - newHeight);
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        // center right
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newWidth = width + dx;
+
+              setState(() {
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        // bottom right
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left + width - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = (dx + dy) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        // bottom center
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newHeight = height + dy;
+
+              setState(() {
+                height = newHeight > 0 ? newHeight : 0;
+              });
+            },
+          ),
+        ),
+        // bottom left
+        Positioned(
+          top: top + height - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var mid = ((dx * -1) + dy) / 2;
+
+              var newHeight = height + 2 * mid;
+              var newWidth = width + 2 * mid;
+
+              setState(() {
+                left = left - 2*mid;
+                height = newHeight > 0 ? newHeight : 0;
+                width = newWidth > 0 ? newWidth : 0;
+              });
+            },
+          ),
+        ),
+        //left center
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              var newWidth = width - dx;
+
+              setState(() {
+                width = newWidth > 0 ? newWidth : 0;
+                left = left + dx;
+              });
+            },
+          ),
+        ),
+        // center center
+        Positioned(
+          top: top + height / 2 - ballDiameter / 2,
+          left: left + width / 2 - ballDiameter / 2,
+          child: ManipulatingBall(
+            onDrag: (dx, dy) {
+              setState(() {
+                isSelected = true;
+                top = top + dy;
+                left = left + dx;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ManipulatingBall extends StatefulWidget {
+  const ManipulatingBall({super.key, required this.onDrag});
+
+  final Function onDrag;
+
+  @override
+  _ManipulatingBallState createState() => _ManipulatingBallState();
+}
+
+class _ManipulatingBallState extends State<ManipulatingBall> {
+  double? initX;
+  double? initY;
+
+  _handleDrag(details) {
+    setState(() {
+      initX = details.globalPosition.dx;
+      initY = details.globalPosition.dy;
+    });
+  }
+
+  _handleUpdate(details) {
+    var dx = details.globalPosition.dx - initX;
+    var dy = details.globalPosition.dy - initY;
+    initX = details.globalPosition.dx;
+    initY = details.globalPosition.dy;
+    widget.onDrag(dx, dy);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanStart: _handleDrag,
+      onPanUpdate: _handleUpdate,
+      child: Container(
+        width: ballDiameter,
+        height: ballDiameter,
+        decoration: const BoxDecoration(
+          //color: Colors.cyan.withOpacity(0.1),
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
